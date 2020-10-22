85ad4d6654b75ae2b7557b931703ae4a61a41184
vkfbmzef <monkeylordjunior@gmail.com>
2020-10-22 13:02:26 -0700

    complete

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+
+module.exports = router;
+
+router.use(require('./partyRoutes'));
+
+router.use(require('./voterRoutes'));
